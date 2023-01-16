d9a2876f31382022cd718af9c9c11db48dce4e15
Siwon <73284904+tldnjs324@users.noreply.github.com>
2023-01-17 00:01:53 +0900

    2805

--- SSAFY9_11_Algorithm_Study/src/Solution_2805.java
+++ SSAFY9_11_Algorithm_Study/src/Solution_2805.java
@@ -14,7 +14,7 @@ public class Solution_2805 {
 			//0~n/2행 계산
 			for(int i = N/2; i>=0; i--) {
 				String farm = sc.next();//각 행의 농장 작물 수 입력
-				for(int j = i; j<N-i; j++) {
+				for(int j = i; j<=N-1-i; j++) {
 					sum += farm.charAt(j)-'0';//charAt()-'0'으로 int형변환
 				}
 			}
@@ -22,7 +22,7 @@ public class Solution_2805 {
 			//n/2+1~n-1행 계산
 			for(int i = 1; i<=N/2; i++) {
 				String farm = sc.next();
-				for(int j = i; j<N-i; j++) {
+				for(int j = i; j<=N-1-i; j++) {
 					sum += farm.charAt(j)-'0';
 				}
 			}
