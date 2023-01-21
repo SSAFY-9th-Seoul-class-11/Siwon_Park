f22e7b755514bc87586d913f293dca3a43d9e162
Siwon <73284904+tldnjs324@users.noreply.github.com>
2023-01-21 23:59:33 +0900

    2839

---
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Solution_2839.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Solution_2839 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		
+	}
+
+}
