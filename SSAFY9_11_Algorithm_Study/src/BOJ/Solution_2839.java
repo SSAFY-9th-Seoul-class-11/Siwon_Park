d0b9d109f7894aa53d003c05b5f2001dd77b513f
Siwon <73284904+tldnjs324@users.noreply.github.com>
2023-01-22 23:58:53 +0900

    Merge branch 'main' of https://github.com/SSAFY-9th-Seoul-class-11/Siwon_Park.git into main

--- SSAFY9_11_Algorithm_Study/src/BOJ/Solution_2839.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Solution_2839.java
@@ -8,7 +8,22 @@ public class Solution_2839 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		int bag = 0;
 		
+		while(true) {
+			if(N%5 == 0) {
+				bag += N/5;
+				System.out.println(bag);
+				break;
+			}else {
+				N -= 3;
+				bag++;
+			}
+			if(N<0) {
+				System.out.println(-1);
+				break;
+			}
+		}
 		
 	}
 
