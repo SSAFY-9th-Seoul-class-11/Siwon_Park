f7161d98f42bd2c45ce506bb4f448709a94549b5
Siwon <73284904+tldnjs324@users.noreply.github.com>
2023-01-24 00:00:23 +0900

    1225

---
+++ SSAFY9_11_Algorithm_Study/src/SWEA/Solution_1225.java
@@ -0,0 +1,51 @@
+package SWEA;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Solution_1225 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+				
+		for(int tc = 1; tc<=10; tc++) {
+			int T = sc.nextInt();
+			
+			//큐 입력받기
+			Queue<Integer> code = new LinkedList<Integer>();
+			for(int i = 0; i<8; i++) {
+				int c = sc.nextInt();
+				code.add(c);
+			}
+			
+			boolean coding = true;
+			int c = 0;
+			while(coding == true) {
+				for(int i = 1; i<=5; i++) {
+					c = code.poll();
+					c -= i;
+					if(c <= 0) {
+						c = 0;
+					}
+					code.add(c);
+				}
+			}
+			System.out.print("#" + tc +" ");
+			for(int i = 0 ; i<8; i++) {
+				System.out.print(code.poll() + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+/*
+ * queue
+ * add() =>큐에 값 추가
+ * offer() =>큐에 값 추가
+ * poll() =>큐의 첫번째 값을 반환하고 제거, 비어있다면 null 반환
+ * remove() =>큐의 첫번재 값 제거
+ * clear() =>큐 초기화
+ * peek() =>큐 첫번째 값 참조
+ * */
---
+++ SSAFY9_11_Algorithm_Study/src/SWEA/Solution_2806.java
@@ -0,0 +1,10 @@
+package SWEA;
+
+public class Solution_2806 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
