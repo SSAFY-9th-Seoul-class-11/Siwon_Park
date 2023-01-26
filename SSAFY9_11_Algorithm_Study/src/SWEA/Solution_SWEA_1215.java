bb1b40721dddfe8cb8837544f7acde7e643c775f
박시원 <tldnjs324@naver.com>
2023-01-26 17:49:09 +0900

    swea 1215

--- SSAFY9_11_Algorithm_Study/src/SWEA/Solution_SWEA_1215.java
+++ SSAFY9_11_Algorithm_Study/src/SWEA/Solution_SWEA_1215.java
@@ -1,10 +1,27 @@
 package SWEA;
 
+import java.util.Scanner;
+
 public class Solution_SWEA_1215 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		int[] dEven = {-2, -1, 1, 2};
+//		int[] dOdd = {-2, -1, 1, 2};
+		
+		int T = 10;
+		Scanner sc = new Scanner(System.in);
+		
+		for(int tc = 1; tc<=10; tc++) {
+			int length = sc.nextInt();
+			char[][] map = new char[8][8];
+			for(int i = 0; i<8; i++) {
+				for(int j = 0; j<8; j++) {
+					map[i][j] = sc.next().charAt(0);
+				}
+			}
+			
+		}
+		
 	}
 
 }
