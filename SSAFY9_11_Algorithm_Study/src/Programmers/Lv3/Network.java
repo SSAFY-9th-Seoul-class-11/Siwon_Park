2a2a7aed8a603f5a3df1ae25e13721848f04c5e6
Siwon <73284904+tldnjs324@users.noreply.github.com>
2023-01-31 01:42:29 +0900

    Programmers 네트워크

---
+++ SSAFY9_11_Algorithm_Study/src/Programmers/Lv3/Network.java
@@ -0,0 +1,30 @@
+package Programmers.Lv3;
+
+public class Network {
+
+	public int solution(int n, int[][] computers) {
+		int answer = 0;
+		boolean[] visited = new boolean[n];
+
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				dfs(computers, i, visited);
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+
+	boolean[] dfs(int[][] computers, int i, boolean[] check) {
+		check[i] = true;
+
+		for (int j = 0; j < computers.length; j++) {
+			if (i != j && computers[i][j] == 1 && check[j] == false) {
+				check = dfs(computers, j, check);
+			}
+		}
+		return check;
+	}
+
+}
