81c754a089a8c8ff6a9eea98ee5a7658cbf765bd
Siwon <73284904+tldnjs324@users.noreply.github.com>
2023-02-14 20:38:19 +0900

    BOJ 6209 제자리 멀리뛰기 (미완)

--- SSAFY9_11_Algorithm_Study/src/BOJ/Solution_BOJ_6209.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Solution_BOJ_6209.java
@@ -16,28 +16,32 @@ public class Solution_BOJ_6209 {
 		int n = Integer.parseInt(st.nextToken()); // 작은 돌섬의 수
 		int m = Integer.parseInt(st.nextToken()); // 제거할 수 있는 작은 돌섬의 수
 		
-		int end = 0; // 젤 먼 값
-		int[] dis = new int[n]; // 갇힌섬으로부터 각 작은 돌섬이 떨어져 있는 거리
+		int end = 0; // 젤 먼 거리 값
+		int[] dis = new int[n+1]; // 갇힌섬으로부터 각 작은 돌섬이 떨어져 있는 거리
 		for(int i = 0; i<n; i++) {
 			dis[i] = Integer.parseInt(br.readLine());
-			end = Math.max(end, dis[i]); // 최대값 찾기
 		}
+		dis[n] = d;
+		
+		end = d; // 최대값
 
-		int start = 0; // 젤 가까운 값
+		int start = 0; // 젤 가까운 거리 값
 		int result = 0; // 최소 거리의 최댓값
 		
+		//이진 탐색 수행
 		while(start<=end) {
 			int mid = (start+end)/2; //중간 값
-			int pos = 0;
-			int count = 0;
+			int idx = 0;
+			int count = 0; // 제거할 돌섬 수 
 			for(int i = 1; i<=n; i++) {
-				if(dis[i]-dis[pos] < mid) count++;
-				else pos = i;
+				if(dis[i]-dis[idx] < mid) count++; // 최소거리의 수가 중간 값보다 작으면 돌섬 수를++
+				else idx = i;
 			}
-			if(count > m) {
+			if(count > m) { // 제거할 돌섬 수 보다 많으면 거리 값을 줄여야 함
+				result = mid; // 최소 거리의 최대 값이므로, 여기에서 result를 기록
 				end = mid-1;
-			} else {
-				result = mid;
+			} else { // 제거할 돌섬 수보다 작거나 같으면 거리 값을 길게 해야 함
+				//result = mid; // 최소 거리의 최대 값이므로, 여기에서 result를 기록
 				start = mid+1;
 			}
 		}
