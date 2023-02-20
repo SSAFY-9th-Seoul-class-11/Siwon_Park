5e91995777b1a60b7c517c098f090c197fb48a6b
Siwon <73284904+tldnjs324@users.noreply.github.com>
2023-02-20 19:33:40 +0900

    BOJ 9465 스티커

--- SSAFY9_11_Algorithm_Study/src/BOJ/Solution_BOJ_9465.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Solution_BOJ_9465.java
@@ -1,11 +1,11 @@
-package SWEA;
+package BOJ;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_SWEA_9465 {
+public class Solution_BOJ_9465 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,22 +13,21 @@ public class Solution_SWEA_9465 {
 				
 		for(int tc = 0; tc<T; tc++) {
 			int n = Integer.parseInt(br.readLine());
-			int[][] sticker = new int[2][n];
+			int[][] sticker = new int[2][n+1];
+			int[][] dp = new int[2][n+1];
 			for(int i = 0; i<2; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				for(int j = 0; j<n; j++) {
+				for(int j = 1; j<=n; j++) {
 					sticker[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			sticker[0][0] += sticker[1][1];
-			sticker[1][0] += sticker[0][1];
-			for(int i = 2; i<n; i++) {
-				sticker[0][i] += Math.max(sticker[1][i-1], sticker[1][i-2]);
-				sticker[1][i] += Math.max(sticker[0][i-1], sticker[0][i-2]);
+			dp[0][1] = sticker[0][1];
+			dp[1][1] = sticker[1][1];
+			for(int i = 2; i<=n; i++) {
+				dp[0][i] = Math.max(dp[1][i-1], dp[1][i-2]) + sticker[0][i];
+				dp[1][i] = Math.max(dp[0][i-1], dp[0][i-2]) + sticker[1][i];
 			}
-			
+			System.out.println(Math.max(dp[0][n], dp[1][n]));
 		}
-		
 	}
-
 }
