54acc32f4d40b4402aa23440959aaed3eb1585e2
박시원 <tldnjs324@naver.com>
2023-02-27 15:54:47 +0900

    SWEA 3289 서로소 집합

---
+++ SSAFY9_11_Algorithm_Study/src/SWEA/Solution_SWEA_3289.java
@@ -0,0 +1,60 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution_SWEA_3289 {
+
+	static int n;
+	static int[] parents;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for(int tc = 1; tc<=T; tc++) {
+			StringBuilder sb = new StringBuilder();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			makeSet();
+			int m = Integer.parseInt(st.nextToken());
+			
+			for(int i = 0; i<m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int cal = Integer.parseInt(st.nextToken());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				if(cal == 0) {
+					union(a, b);
+				}else if(cal == 1){
+					if(findSet(a) == findSet(b)) sb.append(1);
+					else sb.append(0);
+				}
+			}
+			System.out.println("#" + tc + " " + sb);
+		}
+	}
+	
+	public static void makeSet() {
+		parents = new int[n+1];
+		for (int i = 1; i <= n; i++) {
+			parents[i] = i; 
+		}
+	}
+
+	public static int findSet(int a) {
+		if(a == parents[a]) return a;
+		return parents[a] = findSet(parents[a]); 
+	}
+	public static boolean union(int a, int b) {
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		if(aRoot == bRoot) return false;
+		
+		parents[bRoot] = aRoot;
+		return true;
+	}
+}
