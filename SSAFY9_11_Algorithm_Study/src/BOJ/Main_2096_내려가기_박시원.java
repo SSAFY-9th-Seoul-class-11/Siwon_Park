1bf239a8911ab9cd0bbe5803815441b6ea5ac9d5
jd06280 <jd06280@naver.com>
2023-03-13 16:26:09 +0900

    BOJ 2096 내려가기

--- SSAFY9_11_Algorithm_Study/src/BOJ/Main_2096_내려가기_박시원.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_2096_내려가기_박시원.java
@@ -14,58 +14,59 @@ public class Main_2096_내려가기_박시원 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][3];
-
+		minCost = new int[N][3];
+		maxCost = new int[N][3];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 3; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = minCost[i][j] = maxCost[i][j ]= Integer.parseInt(st.nextToken());
 			}
 		}
-		minCost = new int[N][3];
-		maxCost = new int[N][3];
+		
 		for (int i = 0; i < 3; i++) {
 			minCost[N - 1][i] = maxCost[N - 1][i] = map[N - 1][i];
 		}
-		int min = Math.min(findMin(0, 0), Math.min(findMin(0, 1), findMin(0, 2)));
-		int max = Math.max(findMax(0, 0), Math.max(findMax(0, 1), findMax(0, 2)));
 		
-		System.out.println(max + " " + min);
+		System.out.println(findMax() + " " + findMin());
 	}
-	//최소, 최댓값 재귀로 찾아주는 함수
+	//위치, 최소, 최대 별로 다음값 찾아주는 함수
 	public static int nextLine(int n, int num, boolean mod) { // 행, 열, mod=true: 최소, false: 최대
 		if(num == 0) { // 0, 1 중에서
 			if(mod) {
-				return Math.min(findMin(n+1, 0), findMin(n+1, 1));
+				return Math.min(minCost[n-1][0], minCost[n-1][1]);
 			}else {
-				return Math.max(findMax(n+1, 0), findMax(n+1, 1));
+				return Math.max(maxCost[n-1][0], maxCost[n-1][1]);
 			}
 		}else if(num == 1) { // 0, 1, 2
 			if(mod) {
-				return Math.min(findMin(n+1, 0), Math.min(findMin(n+1, 1), findMin(n+1, 2)));
+				return Math.min(minCost[n-1][0], Math.min(minCost[n-1][1], minCost[n-1][2]));
 			}else {
-				return Math.max(findMax(n+1, 0), Math.max(findMax(n+1, 1), findMax(n+1, 2)));
+				return Math.max(maxCost[n-1][0], Math.max(maxCost[n-1][1], maxCost[n-1][2]));
 			}			
 		}else{ //(num==2) 1, 2
 			if(mod) {
-				return Math.min(findMin(n+1, 1), findMin(n+1, 2));
+				return Math.min(minCost[n-1][1], minCost[n-1][2]);
 			}else {
-				return Math.max(findMax(n+1, 1), findMax(n+1, 2));
+				return Math.max(maxCost[n-1][1], maxCost[n-1][2]);
 			}
 		}
 	}
 	//dp로 최솟값 찾기
-	public static int findMin(int n, int num) {
-		if(n<N-1) { // 탐색하지 않은 배열만 탐색!
-			minCost[n][num] = map[n][num] + nextLine(n, num, true);
+	public static int findMin() {
+		for(int i = 1; i<N; i++) {
+			minCost[i][0] += nextLine(i, 0, true);
+			minCost[i][1] += nextLine(i, 1, true);
+			minCost[i][2] += nextLine(i, 2, true);
 		}
-		return minCost[n][num];
+		return(Math.min(minCost[N-1][0], Math.min(minCost[N-1][1], minCost[N-1][2])));	
 	}
 	//dp로 최댓값 찾기
-	public static int findMax(int n, int num) {
-		if(n<N-1) { // 탐색하지 않은 배열만 탐색!
-			maxCost[n][num] = map[n][num] + nextLine(n, num, false);
+	public static int findMax() {
+		for(int i = 1; i<N; i++) {
+			maxCost[i][0] += nextLine(i, 0, false);
+			maxCost[i][1] += nextLine(i, 1, false);
+			maxCost[i][2] += nextLine(i, 2, false);
 		}
-		return maxCost[n][num];
+		return(Math.max(maxCost[N-1][0], Math.max(maxCost[N-1][1], maxCost[N-1][2])));	
 	}
-
 }
