583913a3a1474e11d5c1679faa252eaef243002a
ParkSiwon <tldnjs324@naver.com>
2023-03-30 00:48:52 +0900

    practice

--- SSAFY9_11_Algorithm_Study/src/practice/Main_막대만들기_박시원.java
+++ SSAFY9_11_Algorithm_Study/src/practice/Main_막대만들기_박시원.java
@@ -3,17 +3,12 @@ package practice;
 public class Main_막대만들기_박시원 {
 
 	public static void main(String[] args) {
-		int[] stick = {1, 1, 2}; //파랑, 노랑, 빨강
 		int[] f = new int[7];
 		
 		f[1] = 2;
 		f[2] = 5;
-		for(int i = 1; i<=6; i++) {
-			for(int j = 0; j<3; j++) {
-				if(i-stick[j]>0) {
-					f[i] += f[i-1];
-				}
-			}
+		for(int i = 3; i<=6; i++) {
+			f[i] = f[i-1]*2 + f[i-2];
 		}
 		System.out.println(f[6]);
 	}
