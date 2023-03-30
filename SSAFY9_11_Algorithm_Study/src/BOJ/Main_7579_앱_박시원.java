2aa22f73620128bede893a6fd16b97fe0d5f2fa4
박시원 <tldnjs324@naver.com>
2023-03-30 17:22:43 +0900

    .

---
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_7579_앱_박시원.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_7579_앱_박시원 {
+	static int N, M;
+	static int[] m, c;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+	}
+}
