cb42c457abecc05db4176ec1d2b7678d9959ce58
ParkSiwon <tldnjs324@naver.com>
2023-04-04 15:05:11 +0900

    BOJ 1043 거짓말

--- SSAFY9_11_Algorithm_Study/src/BOJ/Main_1043_거짓말_박시원.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_1043_거짓말_박시원.java
@@ -4,67 +4,63 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main_1043_거짓말_박시원 {
 
 	static int N, M, cnt;
-	static boolean[] knowing;
 	static int[] parent;
+	static HashSet<Integer> knowing = new HashSet<>();//진실을 아는 사람들
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken()); // 사람 수
 		M = Integer.parseInt(st.nextToken()); // 파티 수
 		
-		st = new StringTokenizer(br.readLine());
-		int k = Integer.parseInt(st.nextToken());
-		knowing = new boolean[k]; //진실을 아는 사람들
-		for(int i = 0; i<k; i++) {
-			knowing[Integer.parseInt(st.nextToken())] = true;
-		}
-		
 		parent = new int[N + 1]; // 부모 배열
 		for (int i = 1; i <= N; i++) {
 			parent[i] = i; // 사람들의 부모를 각각 자신으로 초기화
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		int k = Integer.parseInt(st.nextToken());
+		
+		if(k != 0) {
+			for(int i = 0; i<k; i++) {
+				knowing.add(Integer.parseInt(st.nextToken()));
+			}
 		}
 		
 		ArrayList<Integer>[] people = new ArrayList[M];
 		for(int i=0; i<M; i++) {
 			people[i] = new ArrayList<>();
 		}
-		int a, b =0;
+
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			if(N > 0) {
-				b = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			people[i].add(a);
+			for(int j = 1; j<n; j++) {
+				int b = Integer.parseInt(st.nextToken());
+				union(a, b);
 				people[i].add(b);
-			}
-			for(int j=1; j<N; j++) {
-				a = Integer.parseInt(st.nextToken());
-				people[i].add(a);
-				union(b, a); // 두명씩 union하면 모두가 같은 parent를 갖게 됨.
-				b = a;
-			}
-		}
-		
-		// 진실을 아는 사람들의 parent는 같이 파티를 참여 했으므로 진실을 아는 사람들
-		int parent;
-		for(int i=1; i<knowing.length; i++) {
-			if(knowing[i]) {
-				knowing[find(i)] = true;
-			}
-		}
-		
-		// 진실을 아는 사람들과 파티를 같이 하지 않았으면 cnt++
-		for(int i=0; i<M; i++) {
-			if(people[i].size() > 0) {
-				parent = find(people[i].get(0));
-				if(!knowing[parent]) cnt++;
 			}
 		}
 
+		for(int i = 0; i<M; i++) {
+			boolean flag = true;
+			for(int p : people[i]) {
+				if(knowing.contains(find(parent[p]))) {
+					flag = false;
+					break;
+				}
+			}
+			if(flag) cnt++;
+		}
 		System.out.println(cnt);
 	}
 	
@@ -78,7 +74,12 @@ public class Main_1043_거짓말_박시원 {
 	public static void union(int a, int b) {
 		a = find(a);
 		b = find(b);
-		if (a != b)
-			parent[b] = a;
+		if(knowing.contains(b)) {
+			int tmp = a;
+			a = b;
+			b = tmp;
+		}
+		parent[b] = a;
+			
 	}
 }
