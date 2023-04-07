8e10cc57eb6c483b4ef2604b5817fe1d1bae64e7
ParkSiwon <tldnjs324@naver.com>
2023-04-07 11:11:34 +0900

    BOJ 9935 문자열폭발

--- SSAFY9_11_Algorithm_Study/src/BOJ/Main_9935_문자열폭발_박시원.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_9935_문자열폭발_박시원.java
@@ -6,37 +6,30 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main_9935_문자열폭발_박시원 {
+	static String str;
 	static char[] bomb;
-	static Stack<Character> st = new Stack<>();
-	static Stack<Character> st2 = new Stack<>();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
-		for(int i = 0; i<s.length(); i++) {
-			st.add(s.charAt(i));
+		
+		String s2 = br.readLine();
+		bomb = new char[s2.length()];
+		for(int i = 0; i<s2.length(); i++) {
+			bomb[i] = s2.charAt(i);
 		}
 
-		s = br.readLine();
-		bomb = new char[s.length()];
-		for(int i = 0; i<s.length(); i++) {
-			bomb[i] = s.charAt(i);
-		}
-
-		int size = st.size();
-		for(int i = 0; i<size; i++) {
-			boolean flag = false;
-			char c = st.peek();
-			for(int j = 0; j<bomb.length; j++) {
-				if(c == bomb[j]) flag = true;
-			}
-			if(flag) st.pop();
-			else st2.add(st.pop());
-		}
 		StringBuilder sb = new StringBuilder();
-		size = st2.size();
-		for(int i = 0; i<size; i++) {
-			sb.append(st2.pop());
+		
+		for(int i = 0; i<s.length(); i++) {
+			sb.append(s.charAt(i));
+			if(sb.length()>=bomb.length) {
+				boolean flag = true;
+				for(int j = 0; j<bomb.length; j++) {
+					if(sb.charAt(sb.length()-bomb.length+j) != bomb[j]) flag = false;
+				}
+				if(flag) sb.delete(sb.length()-bomb.length, sb.length());
+			}
 		}
 		if(sb.length() == 0) System.out.println("FRULA");
 		else System.out.println(sb);
