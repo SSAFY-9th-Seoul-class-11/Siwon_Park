d828cbaf74ac527079b35d6aec60d4bdf3b48ab3
ParkSiwon <tldnjs324@naver.com>
2023-04-23 01:38:14 +0900

    BOJ 20040 사이클게임

--- SSAFY9_11_Algorithm_Study/src/BOJ/Main_20040_사이클게임_박시원.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_20040_사이클게임_박시원.java
@@ -8,36 +8,45 @@ import java.util.StringTokenizer;
 public class Main_20040_사이클게임_박시원 {
 
 	static int[] parent;
+	static boolean isCycle = false;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());//점의 개수
-		int m = Integer.parseInt(st.nextToken());//진행된 차례 수
-		
-		for(int i = 0; i<n; i++) {
+
+		int n = Integer.parseInt(st.nextToken());// 점의 개수
+		int m = Integer.parseInt(st.nextToken());// 진행된 차례 수
+		int result = 0; // 사이클이 처음으로 만들어진 차례의 번호
+
+		parent = new int[n];
+		for (int i = 0; i < n; i++) { //부모를 자기 자신으로 초기화
+			parent[i] = i;
+        }
+		for (int i = 1; i <= m; i++) {
+			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			parent[a] = b;
+			union(a, b);
+			if(isCycle) { //사이클이 만들어졌다면!
+				result = i; //만들어진 회차 저장
+				break;
+			}
 		}
+		System.out.println(result);
 
-	}
-
-	// 부모를 찾아주는 함수
-	public static int find(int x) {
-		if (parent[x] == x) // 부모가 자기 자신이면 본인 리턴
-			return x;
-		else// 아니라면 부모의 부모를 찾아줘서 리턴
-			return parent[x] = find(parent[x]);
 	}
 
 	// 유니온 함수! 원소 a와 b를 합쳐주는 함수
 	public static void union(int a, int b) {
 		a = find(a);
 		b = find(b);
-		if (a != b) // a와 b의 부모가 다르다면
-			parent[b] = a; // b의 부모를 a의 부모와 같게 한다
+		if (a != b) parent[b] = a; // a와 b의 부모가 다르다면 b의 부모를 a의 부모와 같게 한다
+		else isCycle = true; // 부모가 같다면 사이클 만들어짐!
 	}
 
+	// 부모를 찾아주는 함수
+	public static int find(int x) {
+		if (parent[x] == x) return x;// 부모가 자기 자신이면 본인 리턴
+		else return parent[x] = find(parent[x]);// 아니라면 부모의 부모를 찾아줘서 리턴
+	}
 }
