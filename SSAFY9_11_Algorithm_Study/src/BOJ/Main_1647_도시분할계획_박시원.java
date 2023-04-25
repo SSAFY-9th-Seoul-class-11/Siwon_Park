04975a5551b71e50f9a7d20059220fa9934e9970
ParkSiwon <tldnjs324@naver.com>
2023-04-26 08:44:50 +0900

    .

---
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_1647_도시분할계획_박시원.java
@@ -0,0 +1,24 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1647_도시분할계획_박시원 {
+	static int N, M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken()); // 집의 개수
+		M = Integer.parseInt(st.nextToken()); // 길의 개수
+		int A, B, C; // A번 집과 B번 집을 연결하는 길의 유지비가 C 
+		for(int i = 0; i<M; i++) {
+			
+		}
+
+	}
+
+}
