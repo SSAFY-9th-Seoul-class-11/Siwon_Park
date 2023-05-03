f44a5c4ecca9b67d1da69f71b7423a4ef4489b3d
ParkSiwon <tldnjs324@naver.com>
2023-05-04 00:25:15 +0900

    .

---
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_2342_dancedancerevolution_박시원.java
@@ -0,0 +1,24 @@
+package BOJ;
+
+import java.io.IOException;
+
+public class Main_2342_dancedancerevolution_박시원 {
+	
+	static class Pos { // 왼, 오른발 위치, 다음 가야할 위치
+		int x, y, next;//, bx, by;
+		public Pos(int x, int y, int next) {//, int bx, int by
+			super();
+			this.x = x;
+			this.y = y;
+			this.next = next;
+//			this.bx = bx;
+//			this.by = by;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		//중앙: 0, 위: 1, 왼: 2, 아래: 3, 오른: 4
+		//차가 0일 때 : 1힘, 차가 1이고 전이 0일 때 :2힘, 차가 1이고 전 위치가 0이 아닐 때 3힘, 차가 2일 때 4힘
+		//한번에 두 발을 전부 움직이진 못함, 한발씩만 움직이기 가능
+		
+	}
+}
