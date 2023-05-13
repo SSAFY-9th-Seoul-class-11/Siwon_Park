d21b3aaca248911da2d09cc5b6d5ad599c60c702
ParkSiwon <tldnjs324@naver.com>
2023-05-13 23:57:00 +0900

    .

---
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_1406_에디터_박시원.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1406_에디터_박시원 {
+
+	static String str;
+	static int N, M;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		str = br.readLine(); //초기 입력 문자열
+		N = str.length(); // 문자열 길이
+		M = Integer.parseInt(br.readLine()); // 명령어 개수
+		
+		for(int i = 0; i<M; i++) {
+			
+		}
+		
+
+	}
+	
+	public static void edit(String order) {
+		
+	}
+
+}
