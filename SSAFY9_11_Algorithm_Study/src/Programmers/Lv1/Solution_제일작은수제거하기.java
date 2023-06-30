e3d1acc736d3a9cb2de6e9e051c509bacbce1df7
ParkSiwon <tldnjs324@naver.com>
2023-07-01 00:33:23 +0900

    프로그래머스 제일 작은 수 제거하기

--- SSAFY9_11_Algorithm_Study/src/Programmers/Lv1/Solution_제일작은수제거하기.java
+++ SSAFY9_11_Algorithm_Study/src/Programmers/Lv1/Solution_제일작은수제거하기.java
@@ -4,22 +4,21 @@ import java.util.*;
 public class Solution_제일작은수제거하기 {
 	public int[] solution(int[] arr) {
         int[] answer = {-1};
+        if(arr.length==1) return answer;
         
-        if(arr.length!=1) {
-        	int minIdx = 0;
-            for(int i = 1; i<arr.length; i++){
-                if(Math.min(arr[minIdx], arr[i])==arr[i]) {
-                	minIdx = i;
-                }
+        int minIdx = 0;
+        for(int i = 1; i<arr.length; i++){
+            if(Math.min(arr[minIdx], arr[i])==arr[i]) {
+            	minIdx = i;
             }
-            
-        	answer = new int[arr.length-1];
-        	int idx = 0;
-        	for(int i = 0; i<arr.length; i++) {
-            	if(i==minIdx)continue;
-            	answer[idx] = arr[i];
-            	idx++;
-            }
+        }
+        
+    	answer = new int[arr.length-1];
+    	int idx = 0;
+    	for(int i = 0; i<arr.length; i++) {
+        	if(i==minIdx)continue;
+        	answer[idx] = arr[i];
+        	idx++;
         }
         
         return answer;
