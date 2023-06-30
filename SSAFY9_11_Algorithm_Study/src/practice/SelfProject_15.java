b1e40a4f149878d13726e81213237d091d1ccb61
ParkSiwon <tldnjs324@naver.com>
2023-06-30 22:07:09 +0900

    self project 15 collection

--- SSAFY9_11_Algorithm_Study/src/practice/SelfProject_15.java
+++ SSAFY9_11_Algorithm_Study/src/practice/SelfProject_15.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 
-public class LeapYear {
+public class SelfProject_15 {
 	public static void main(String[] args) {
 
 		Calendar cal = Calendar.getInstance();
