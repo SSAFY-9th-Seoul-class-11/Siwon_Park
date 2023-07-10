3319b64c9d98ffa22bcc8fb9c006ab7e05a601fd
ParkSiwon <tldnjs324@naver.com>
2023-07-10 22:45:04 +0900

    프로그래머스 직사각형 별찍기

---
+++ SSAFY9_11_Algorithm_Study/src/Programmers/Lv1/Solution_직사각형별찍기.java
@@ -0,0 +1,18 @@
+package Programmers.Lv1;
+import java.util.*;
+public class Solution_직사각형별찍기 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<b; i++){
+            for(int j = 0; j<a; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
