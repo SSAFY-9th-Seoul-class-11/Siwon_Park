0de1b65e9804abe61ed00ac3c26d3fe5fafd2b49
ParkSiwon <tldnjs324@naver.com>
2023-07-18 23:27:01 +0900

    프로그래머스 예산

---
+++ SSAFY9_11_Algorithm_Study/src/Programmers/Lv1/Solution_예산.java
@@ -0,0 +1,15 @@
+package Programmers.Lv1;
+import java.util.*;
+
+public class Solution_예산 {
+	public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i = 0; i<d.length; i++){
+            budget-=d[i];
+            if(budget<0) break;
+            answer++;
+        }
+        return answer;
+    }
+}
