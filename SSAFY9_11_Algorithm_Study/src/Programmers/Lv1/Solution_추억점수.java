0f7147adf12e6a2ab5a715cc3c1a6442cb616270
ParkSiwon <tldnjs324@naver.com>
2023-07-31 23:18:13 +0900

    프로그래머스 추억점수

---
+++ SSAFY9_11_Algorithm_Study/src/Programmers/Lv1/Solution_추억점수.java
@@ -0,0 +1,18 @@
+package Programmers.Lv1;
+
+public class Solution_추억점수 {
+	public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        for(int i = 0; i<photo.length; i++){
+            for(int j = 0; j<photo[i].length; j++){
+                for(int k = 0; k<name.length; k++){
+                    if(photo[i][j].equals(name[k])){
+                        answer[i] += yearning[k];
+                        break;
+                    }
+                }
+            }
+        }        
+        return answer;
+    }
+}
