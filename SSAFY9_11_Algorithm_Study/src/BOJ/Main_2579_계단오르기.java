6b6b79975e7da365d315ea8184a060fabf72217e
ParkSiwon <tldnjs324@naver.com>
2023-08-07 23:05:05 +0900

    BOJ 계단 오르기

--- SSAFY9_11_Algorithm_Study/src/BOJ/Main_2579_계단오르기.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_2579_계단오르기.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_계단오르기 {
+public class Main_2579_계단오르기 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
