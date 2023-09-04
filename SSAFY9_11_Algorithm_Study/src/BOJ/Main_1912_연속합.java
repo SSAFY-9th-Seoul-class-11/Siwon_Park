25725633f95c8bb00a2e897ace15d591e9c47924
ParkSiwon <tldnjs324@naver.com>
2023-09-05 00:23:00 +0900

    BOJ 연속합(최적화)

--- SSAFY9_11_Algorithm_Study/src/BOJ/Main_1912_연속합.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_1912_연속합.java
@@ -10,16 +10,16 @@ public class Main_1912_연속합 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine()); // 수열 길이
 		StringTokenizer st = new StringTokenizer(br.readLine()); // 수열
-		int[][] dp = new int[n+1][n+1]; // 첫번째 배열 인덱스가 시작 숫자 인덱스, 두번째가 끝 인덱스
+		int[] num = new int[n+1];
+		int[] dp = new int[n+1]; // 인덱스까지 왔을 때 최대값
 		for(int i = 1; i<=n; i++) {
-			dp[i][1] = Integer.parseInt(st.nextToken());
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		int max=dp[1][1];
-		for(int i = 1; i<=n; i++) {
-			for(int j = i+1; j<=n; j++) {
-				dp[i][j] = dp[i][j-1]+dp[j][1];
-				max = Math.max(max, dp[i][j]);
-			}
+		dp[1] = num[1];
+		int max = num[1];
+		for(int i = 2; i<=n; i++) {
+			dp[i] = Math.max(dp[i-1]+num[i], num[i]);
+			max = Math.max(max, dp[i]);
 		}
 		System.out.println(max);
 	}
