c65d3107e76a5482753180d00721c8c23897cfa4
ParkSiwon <tldnjs324@naver.com>
2023-09-07 22:27:12 +0900

    boj 트리의 부모찾기

--- SSAFY9_11_Algorithm_Study/src/BOJ/Main_11725_트리의부모찾기.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_11725_트리의부모찾기.java
@@ -3,44 +3,46 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main_11725_트리의부모찾기 {
-	static boolean[][] node;
+	static ArrayList<Integer>[] node;
+	static boolean[] visited;
 	static int[] parent;
 	static int N;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
-		node = new boolean[N+1][N+1];
+		node = new ArrayList[N+1];
+		visited = new boolean[N+1];
 		parent = new int[N+1];
+		
+		for(int i = 1; i<=N; i++) {
+			node[i] = new ArrayList<>();
+		}
+		
 		int a = 0; int b = 0;
 		for(int i = 1; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
-			node[a][b] = true;
-			node[b][a] = true;
+			node[a].add(b);
+			node[b].add(a);
 		}
-		for(int i = 1; i<=N; i++) {
-			if(node[1][i]) {
-				parent[i] = 1;
-				dfs(i);
-			}
-		}
+		dfs(1);
 		for(int i = 2; i<=N; i++) {
 			System.out.println(parent[i]);
 		}
 		
 	}
 	public static void dfs(int num) {
-		for(int i = 1; i<=N; i++) {
-			if(i==parent[num]) continue;
-			if(node[num][i]) {
-				parent[i] = num;
-				dfs(i);
-			}
+		visited[num] = true;
+		for(int i : node[num]) {
+			if(visited[i]) continue;
+			parent[i] = num;
+			dfs(i);
 		}
 	}
 }
