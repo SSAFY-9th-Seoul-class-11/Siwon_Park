0edafaa244bdc81fb7d0741bab119e13949d38a4
ParkSiwon <tldnjs324@naver.com>
2023-09-13 23:23:42 +0900

    BOJ 보물

--- SSAFY9_11_Algorithm_Study/src/BOJ/Main_1026_보물.java
+++ SSAFY9_11_Algorithm_Study/src/BOJ/Main_1026_보물.java
@@ -3,17 +3,17 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_1026_보물 {
-	static int N, min = Integer.MAX_VALUE;
-	static int[] A, B, selected;
-	static boolean[] visited;
+	static int N;
+	static int[] A, B;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
-		A = new int[N];	B = new int[N]; selected = new int[N]; visited = new boolean[N];
+		A = new int[N];	B = new int[N]; 
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i<N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
@@ -22,26 +22,14 @@ public class Main_1026_보물 {
 		for(int i = 0; i<N; i++) {
 			B[i] = Integer.parseInt(st.nextToken());
 		}
-		dfs(0);
-		System.out.println(min);
-	}
-	public static void dfs(int cnt) {
-		if(cnt == N) {
-			min = Math.min(min, sum());
-			return;
-		}
-		for(int i = 0; i<N; i++) {
-			if(visited[i]) continue;
-			visited[i] = true;
-			selected[cnt] = A[i];
-			dfs(cnt+1);
-			visited[i] = false;
-		}
+		Arrays.sort(A);
+		Arrays.sort(B);
+		System.out.println(sum());
 	}
 	public static int sum() {
 		int sum = 0;
 		for(int i = 0; i<N; i++) {
-			sum += selected[i]*B[i];
+			sum += A[i]*B[N-i-1];
 		}
 		return sum;
 	}
